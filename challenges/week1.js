c5534e8ecdf176c321ebeb745ee14d6545236cec
David Underwood <dunderwood13@hotmail.co.uk>
2019-03-23 07:08:10 +0000

    removed commented out code

--- challenges/week1.js
+++ challenges/week1.js
@@ -16,8 +16,6 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  //var totalPrice = (originalPrice * ((100 + vatRate) / 100)).toFixed(2);
-  //totalPrice = parseFloat(totalPrice);
   return parseFloat((originalPrice * ((100 + vatRate) / 100)).toFixed(2)); 
 }
 
